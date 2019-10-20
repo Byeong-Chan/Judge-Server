14f415a598927866deb5455ca0b4936f9c153909
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-10-21 06:55:23 +0900

    initialize ready to judge system

--- judge_server/app.js
+++ judge_server/app.js
@@ -1,19 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var stylus = require('stylus');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const stylus = require('stylus');
+const cors = require('cors'); //TODO: 개발이 모두 끝나면 지워주세요.
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const judgeQueueRouter = require('./routes/judgeQueue');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
+app.use(cors()); // TODO: 개발이 모두 끝나면 지워주세요.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,8 +22,7 @@ app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/judge', judgeQueueRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,5 +39,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
---
+++ judge_server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'mongodbUri' : 'mongodb://127.0.0.1:27017/localtest',
+    'serverNumber' : 1 // TODO: 서버를 증설할때마다 증설한 서버끼리 서로 다른 번호여야 합니다.
+}
