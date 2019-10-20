51062772ddc9c6a76c821098df2c57fdbca0df3c
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-10-21 07:50:51 +0900

    implementation pushed new submission

--- judge_server/routes/judgeQueue.js
+++ judge_server/routes/judgeQueue.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const pushQueue = require('../pushQueue');
 
-router.get('/', function(req, res, next) {
-    console.log(req.app.get('judgeQueue'));
+const bodyParser = require('body-parser');
+const model = require('../model/model');
+
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+router.post('/', function(req, res, next) {
+    const Queue = req.app.get('judgeQueue');
+    model.judgeQueue.findOne()
+        .where('pending_number').equals(req.body.pending_number)
+        .then(result => {
+            if(!result) throw new Error('none-pending');
+            pushQueue(Queue, result);
+            res.status(200).send('Success');
+        }).catch(err => {
+            console.log(err);
+            res.status(500).send('fail');
+    });
 });
 
 module.exports = router;
