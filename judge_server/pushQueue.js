a9fa378342df51322daad14650eb6e51be21c95d
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-10-21 09:11:08 +0900

    fix critical issue

--- judge_server/pushQueue.js
+++ judge_server/pushQueue.js
@@ -15,17 +15,37 @@ const Compiler = {
         fs.writeFileSync('../tester/test.c', user_code, 'utf8');
         const script = 'docker run --rm -v' + ' ' + testerDir + ':' + dockerDir + ' ' +
             'gcc gcc /home/test.c -o /home/test.o -O2 -Wall -lm -static -std=c99 -DONLINE_JUDGE -DBOJ';
-        const stdout = execSync(script);
 
-        return stdout;
+        let ret = { success: false, stdout: ''};
+        try {
+            const stdout = execSync(script).toString();
+            ret.success = true;
+            ret.stdout = stdout;
+        } catch(exception) {
+            const stdout = exception.stderr.toString();
+            ret.success = false;
+            ret.stdout = stdout;
+        }
+
+        return ret;
     },
     'cpp': function(user_code) {
         fs.writeFileSync('../tester/test.cc', user_code, 'utf8');
         const script = 'docker run --rm -v' + ' ' + testerDir + ':' + dockerDir + ' ' +
             'gcc g++ /home/test.cc -o /home/test.o -O2 -Wall -lm -static -std=gnu++17 -DONLINE_JUDGE -DBOJ';
-        const stdout = execSync(script);
 
-        return stdout;
+        let ret = { success: false, stdout: '' };
+        try {
+            const stdout = execSync(script).toString();
+            ret.success = true;
+            ret.stdout = stdout;
+        } catch(exception) {
+            const stdout = exception.stderr.toString();
+            ret.success = false;
+            ret.stdout = stdout;
+        }
+
+        return ret;
 
     },
     'java': function(user_code) {
@@ -47,9 +67,11 @@ const seccomp_rule = function(lang) {
     }
 };
 
-const status_set = function(status_number, pending_number) {
+const state_set = function(state_number, pending_number, error_message) {
     model.judge.where('pending_number').equals(pending_number)
-        .update({$set: {status: status_number}}).then(result => {
+        .update({$set: {state: state_number, ErrorMessage: error_message}}).then(result => {
+            //TODO: 로깅할것인가?
+            console.log(result);
         return model.judgeQueue.where('pending_number').equals(pending_number)
             .deleteOne();
     }).then(result => {
@@ -59,11 +81,13 @@ const status_set = function(status_number, pending_number) {
         //TODO: database error도 로깅할것인가?
         console.log(err);
     });
-}
+};
 
 
 const pushQueue = function(Queue, judgeObj) {
     let user_code = "";
+    let user_lang = undefined;
+    let error_message = "";
     const max_process_number = 200; // TODO: 나중에 고쳐주세요.
     const max_output_size = 2097152; // TODO: 나중에 고쳐주세요.
     Queue.place(function() {
@@ -72,17 +96,19 @@ const pushQueue = function(Queue, judgeObj) {
             .then(result => {
                 if(!result) throw new Error('none-pending');
                 user_code = result.code;
+                user_lang = result.language;
                 return model.problem.findOne()
                     .where('problem_number').equals(result.problem_number);
             }).then(result => {
                 if(!result) throw new Error('none-problem');
-                if(Compiler[result.language] === undefined) throw new Error('none-language');
+                if(Compiler[user_lang] === undefined) throw new Error('none-language');
 
-                const lang = result.language;
+                const lang = user_lang;
 
-                const errMessage = Compiler[lang](user_code);
+                const errCheck = Compiler[lang](user_code);
 
-                if(errMessage.length > 0) throw new Error('Compile-Error\n' + errMessage);
+                error_message = errCheck.stdout;
+                if(!errCheck.success) throw new Error('Compile-Error\n' + errCheck.stdout);
 
                 for(let i = 0; i < result.input_list.length; i++) {
                     fs.writeFileSync('../tester/input.txt', result.input_list[i].txt, 'utf8');
@@ -96,30 +122,30 @@ const pushQueue = function(Queue, judgeObj) {
                         '--output_path="/home/output.txt"' + ' ' + '--error_path="/home/error.txt"' + ' ' + '--uid=0' + ' ' +
                         '--gid=0' + ' ' + '--seccomp_rule_name=' + seccomp_rule(lang);
 
-                    const stdout = execSync(script);
+                    const stdout = execSync(script).toString();
                     const status = JSON.parse(stdout);
 
-                    // TODO: 개별 채점 해야하는가? 백준식 채점이 좋은가? 구현하려면 스키마를 바꿔야합니다.
-                    if(status.result == 4) {
-                        if(status.signal == 25) throw new Error('Presentation-Error');
+                    // TODO: 개별 채점 해야하는가? 백준식 채점이 좋은가? 개별 채점 구현하려면 스키마를 바꿔야합니다.
+                    if(status.result === 4) {
+                        if(status.signal === 25) throw new Error('Presentation-Error');
                         else throw new Error('Runtime-Error');
                     }
-                    else if(status.result == 1 || status.result == 2) throw new Error('Timelimit-Error');
-                    else if(status.result == 3) throw new Error('Memorylimit-Error');
-                    else throw new Error('Server-Error');
+                    else if(status.result === 1 || status.result === 2) throw new Error('Timelimit-Error');
+                    else if(status.result === 3) throw new Error('Memorylimit-Error');
+                    else if(status.result !== 0) throw new Error('Server-Error');
 
-                    const user_output = fs.readFileSync('../tester/output.txt', result.input_list[i].txt, 'utf8').toString();
+                    const user_output = fs.readFileSync('../tester/output.txt', 'utf8').toString();
                     const system_output = result.output_list[i].txt;
 
                     const user_output_cmp = user_output.split('\n');
                     const system_output_cmp = system_output.split('\n');
 
                     while(user_output_cmp[user_output_cmp.length - 1].trimEnd() === '') {
-                        user_output_cmp.split(user_output_cmp.length - 1);
+                        user_output_cmp.splice(user_output_cmp.length - 1);
                     }
 
                     while(system_output_cmp[system_output_cmp.length - 1].trimEnd() === '') {
-                        system_output_cmp.split(system_output_cmp.length - 1);
+                        system_output_cmp.splice(system_output_cmp.length - 1);
                     }
 
                     if(system_output_cmp.length !== user_output_cmp.length) throw new Error('Wrong-Answer');
@@ -128,13 +154,17 @@ const pushQueue = function(Queue, judgeObj) {
                         if(system_output_cmp[j].trimEnd() !== user_output_cmp[j].trimEnd()) throw new Error('Wrong-Answer');
                     }
                 }
+                console.log(judgeObj.pending_number);
+                state_set(2, judgeObj.pending_number, error_message);
                 Queue.next();
             }).catch(err => {
                 if(typeof err.message === 'undefined') {
+                    console.log('Hello undefined');
                     // TODO: 채점 sever 문제는 중대하므로 로깅으로 나중에 꼭 고칠 것
                     console.log(err);
                 }
-                else if(typeof err.message !== 'String') {
+                else if(typeof err.message !== 'string') {
+                    console.log(typeof err.message);
                     // TODO: 채점 sever 문제는 중대하므로 로깅으로 나중에 꼭 고칠 것
                     console.log(err);
                 }
@@ -142,37 +172,37 @@ const pushQueue = function(Queue, judgeObj) {
                     /***
                      * 컴파일 에러
                      */
-                    status_set(8, judgeObj.pending_number);
+                    state_set(8, judgeObj.pending_number, error_message);
                 }
                 else if(err.message === 'Wrong-Answer') {
                     /***
                      * 오답
                      */
-                    status_set(3, judgeObj.pending_number);
+                    state_set(3, judgeObj.pending_number, error_message);
                 }
                 else if(err.message === 'Timelimit-Error') {
                     /***
                      * 시간 초과
                      */
-                    status_set(4, judgeObj.pending_number);
+                    state_set(4, judgeObj.pending_number, error_message);
                 }
                 else if(err.message === 'Memorylimit-Error') {
                     /***
                      * 메모리 초과
                      */
-                    status_set(5, judgeObj.pending_number);
+                    state_set(5, judgeObj.pending_number, error_message);
                 }
                 else if(err.message === 'Runtime-Error') {
                     /***
                      * 런타임 에러
                      */
-                    status_set(6, judgeObj.pending_number);
+                    state_set(6, judgeObj.pending_number, error_message);
                 }
                 else if(err.message === 'Presentation-Error') {
                     /***
                      * 출력초과
                      */
-                    status_set(7, judgeObj.pending_number);
+                    state_set(7, judgeObj.pending_number, error_message);
                 }
                 else {
                     //TODO: 채점 server 문제는 중대하므로 로깅으로 나중에 꼭 고칠 것
